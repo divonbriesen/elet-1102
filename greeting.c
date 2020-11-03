1a18fe46b7667e69047fde0089d8ba14b0143218
D.I. von Briesen <divonbriesen@gmail.com>
2020-11-03 13:34:12 -0500

    Create greeting.c
    pasted standard hello world code in c

---
+++ greeting.c
@@ -0,0 +1,12 @@
+/******************************************************************************
+a sample basic c file for the reason of showing github
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World");
+
+    return 0;
+}
